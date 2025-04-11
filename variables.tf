ac7e06567db0efc3e2dbbbfb8fc22c7ebb3416b6
damiend <damien.dutech@stack-labs.com>
2025-04-11 12:20:10 +0200

    first commit

---
+++ variables.tf
@@ -0,0 +1,46 @@
+#########################
+### Default variables ###
+#########################
+variable "region" {
+  type = string
+}
+
+variable "infra_provider" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project_application" {
+  type = string
+}
+
+variable "ressource_name" {
+  type = string
+}
+
+variable "unique_id" {
+  type = string
+}
+
+########################
+### Module variables ###
+########################
+
+variable "aws_iam_role_id" {
+  type = list
+}
+
+variable "aws_iam_policy_attachment_name" {
+  type = string
+}
+
+variable "aws_iam_policy_attachment_policy_arn" {
+  type = string
+}
