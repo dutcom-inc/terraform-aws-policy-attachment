5b22c17987db532f56a0496659e2240e4c5c1344
damiend <damien.dutech@stack-labs.com>
2025-04-16 15:56:50 +0200

    add - multi var

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy_attachment" "default" {
     name = var.aws_iam_policy_attachment_name
-    roles = [var.aws_iam_role_id]
+    roles = var.aws_iam_role_ids
     policy_arn = var.aws_iam_policy_attachment_policy_arn
 }
--- variables.tf
+++ variables.tf
@@ -33,8 +33,8 @@ variable "unique_id" {
 ### Module variables ###
 ########################
 
-variable "aws_iam_role_id" {
-  type = string
+variable "aws_iam_role_ids" {
+  type = list(string)
 }
 
 variable "aws_iam_policy_attachment_name" {
